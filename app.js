fd9f022505a0fc4cb88e0da030c2cf448b5a4e5d
Amancayjesus136 <amancayjesus136@gmail.com>
2023-10-27 18:55:33 -0500

    cors npm

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-const port = 3000;
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const port = 3000
 
-mongoose.connect('mongodb://0.0.0.0:27017/llego_bd', {
-});
+var cors = require('cors');
+app.use(cors({origin: 'http://localhost:8100'}))
 
-const database = mongoose.connection;
-database.on('error', (error) => {
-  console.log('Hubo un error:', error);
-});
-database.once('open', () => {
-  console.log('Base de datos conectada');
-});
+mongoose.connect("mongodb://0.0.0.0:27017/llego_bd")
+const database = mongoose.connection
+database.on('error',(error) => {
+    console.log('hubo un error', error)
+})
 
-const eventos = require('./routes/eventos');
-app.use('/eventos', eventos);
+database.once('connected', () => {
+    console.log('base de datos conectada')
+})
 
-app.listen(port, () => {
-  console.log('Ingreso a http://localhost:3000');
-});
+const eventos = require ('./routes/eventos')
+app.use('/eventos', eventos)
+
+app.listen(port, () =>{
+    console.log('Ingresa a localhost:3000')
+})
